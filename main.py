bd0e3cd37a14b17a259c28fc8e3bfe9c8de5cd2a
Georgiy-Kinyov <magister.kinyov@yandex.ru>
2024-05-15 23:05:11 +0500

    Service test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("I am the very model of a modern Roman emperor")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/test")
+def test():
+    return "I am the very model of a modern Roman emperor"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
