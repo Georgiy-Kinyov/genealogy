0ddf5df34846a5b2d7711d2c37dbbf8f8a1175e1
Georgiy-Kinyov <magister.kinyov@yandex.ru>
2024-05-27 18:49:03 +0500

    Removed debugging

--- main.py
+++ main.py
@@ -130,10 +130,5 @@ def search_child(id):
     return jsonify({"data": res})
 
 
-@app.route("/test")
-def test():
-    return "I am the very model of a modern Roman emperor"
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=False)
